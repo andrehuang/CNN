86db205a409d37ccaf038846d326ac72b0d52487
Haiwen Huang <smshhw@pku.edu.cn>
2017-11-04 15:07:53 +0800

    Update mask_op.py

--- mask_op.py
+++ mask_op.py
@@ -31,75 +31,78 @@ def py_func(func, inp, tout, stateful=True, name=None, grad=None):
     with g.gradient_override_map({"PyFunc": rnd_name}):
         return tf.py_func(func, inp, tout, stateful=stateful, name=name)
 
-# posTemp
+
 def getMask(inputs):
     '''
-    :param inputs: should be a tensor (last layer's output)
+    :param inputs:  a numpy array (When wrapped in a tensorflow function, inputs is a Tensor)
     :return: mask, which is a tensor corresponding to that tensor
     '''
-    global filter_type   # should be defined in our_cnn_mnist.py file
-
-    shape = inputs.get_shape().as_list()  # [batch_size, 7, 7, 64] in mnist example
+    # global filter_type   # should be defined in our_cnn_mnist.py file
+    shape = inputs.shape
     batchS, h, w, depth = shape
 
-    # checked
-    def getMu(input=inputs):
+    def getMu(inpu=inputs):
         '''
         :param input: tensor , the layer
-        :return: mu_x, mu_y, sqrtvar; (mu_x, mu_y) is the location of the 
+        :return: mu_x, mu_y; (mu_x, mu_y) is the location of the mu;
+                the shapes of mu_x, mu_y are both [batchS, 1, 1, depth]
         '''
-        # 几个注意点：
-        # 1 Python中的index从0开始
-        # 2 关于shape, MATLAB中采用[h, w, depth, batchS]
-        #   python中采用[batchS, h, w, depth]
-        #   相应的sum的axis也要调整(from trial, np.sum(z,1) is equivalent to sum(z, 2) in MATLAB
-        # 3 np.arange(1, h+1) 与 MATLAB中(1:h)是一样的，这也是python的特点
-        # 4 shape的数字应该还是没问题的，实验了一下，[3,3]就是3*3
-        # 5 延续上一点，与shape相关的index都是natural的理解方式，比如还有在np.tile中给出的参数。
+
         nonlocal batchS, h, w, depth, shape
-        # 下面是对MATLAb代码的修改
-        # flatten depth-wise
-        layer = tf.reshape(input, [batchS, h*w, depth])
-        # constructs templates
-        tmp_x = np.array(np.tile(np.transpose(np.arange(1, h+1)), [batchS, w, depth]))
-        tmp_y = np.array(np.reshape(np.tile(np.arange(1, w+1), [batchS, h, 1, depth]), [batchS, h * w, depth]))
 
-        sumX = np.maximum(np.sum(layer, 0), 0.000000001)
+        emp = np.zeros([batchS, h, w, depth])
+        tmp_x = emp + np.array(np.reshape(np.arange(1, h+1), newshape=(1, h, 1, 1)))
+        tmp_y = emp + np.array(np.reshape(np.arange(1, w + 1), newshape=(1, 1, w, 1)))
+        # Now both tmp_x, tmp_y both have shape (batchS, h, w, depth)
 
-        mu_x = np.maximum(np.round(np.divide(np.sum(np.multiply(tmp_x, layer), 0), sumX)), 1)
-        mu_y = np.maximum(np.round(np.divide(np.sum(np.multiply(tmp_y, layer), 0), sumX)), 1)
-        sqrtvar = np.sqrt(
-                    np.divide((np.sum(np.multiply(np.square(tmp_x - np.tile(mu_x, [1, h * w, 1])), layer), 0) +
-                           np.sum(np.multiply(np.square(tmp_y - np.tile(mu_y, [1, h * w, 1])), layer), 0)), sumX))
+        sumX = np.maximum(np.sum(inpu, axis=(1, 2)), 0.000000001)
+        # shape of sumX: [batchS, depth]
+        tx = np.sum(np.multiply(tmp_x, inpu), (1, 2))
+        ty = np.sum(np.multiply(tmp_y, inpu), (1, 2))
+        # shape of tx, ty: [batchS, depth]
+        mu_x = np.maximum(np.round(np.divide(tx, sumX)), 1)
+        mu_y = np.maximum(np.round(np.divide(ty, sumX)), 1)
+        # shape of mu_x, mu_y: [batchS, depth]
 
-        np.delete(arr=[sumX, tmp_x, tmp_y])
+        # 想要以mu_x, mu_y的各个值为index来取出tmp的元素
+        # tmp = np.linspace(-1, 1, h)
+        # 肯定可以搞，但暂时没想出怎么用array indexing；不过发现可以直接数学地解决它
 
-        # It seems the next two lines of codes are not used, I commented them
-        # [maxX, ~] = max(x, [], 1)
-        # p = np.tile(np.mutilply(mu_x + (mu_y - 1),h), [batchS, 1, 1, depth])
+        # rescaling
+        # Notice that mu_x and mu_y are integers in the range [1, h]
+        mu_x = -1 + mu_x * 2/h
+        mu_y = -1 + mu_y * 2/h
+        # shape of mu_x, mu_y: [batchS, depth]
+        mu_x = mu_x.reshape((batchS, 1, 1, depth))
+        mu_y = mu_y.reshape((batchS, 1, 1, depth))
+        return mu_x, mu_y
 
-        tmp = np.linspace(-1, 1, h)
-        mu_x = np.tile(tmp[mu_x], [batchS, 1, 1, depth])
-        mu_y = np.tile(tmp[mu_y], [batchS, 1, 1, depth])
-        sqrtvar = np.tile(sqrtvar, [batchS, depth])
-        return mu_x, mu_y, sqrtvar
+    mu_x, mu_y = getMu(inputs)
+    # mu_x, mu_y 's shapes are both (batchS, 1, 1, depth)
 
-    mu_x, mu_y, sqrtvar = getMu(inputs)
+    # 设一个index作为全局变量，每一层存一个对应的posTemp
+    lx = np.linspace(-1, 1, w)
+    lx = np.reshape(lx, newshape=(1, 1, w, 1))
+    ly = np.transpose(np.linspace(-1, 1, h))
+    ly = np.reshape(ly, newshape=(1, h, 1, 1))
+    empTemp = np.zeros((batchS, h, w, depth))
+    posTempX = empTemp + lx
+    posTempY = empTemp + ly
 
-    posTemp = []  # 让学长来加一下
-
-    posTempX = np.tile(posTemp[0], [batchS, 1, 1, 1])
-    posTempY = np.tile(posTemp[1], [batchS, 1, 1, 1])
-
-    mask = np.absolute(posTempX - np.reshape(mu_x, [h, w, 1]))  # 对这里[h,w,1]的意义不太确定，不过我觉得1就是指depth，这里不需要batchS参数
-    mask = mask + np.absolute(posTempY - np.reshape(mu_y, [h, w, 1]))
+    mu_x = empTemp + mu_x
+    mu_y = empTemp + mu_y
+    # mu_x, mu_y 's shapes are both [batchS, 1, 1, depth]
+    mask = np.absolute(posTempX - mu_x)
+    mask = np.add(mask, np.absolute(posTempY - mu_y))
     mask = np.maximum(1 - np.multiply(mask, 2), -1)
 
     # 这些细节需要和学长一起修改：
     # partRate。在大的层面里设置一个与filter_type(partRate)。固定的参数。
     # filter_type应该要被加在our_mnist里面，代表是否需要mask。
-    mask[:, :, filter_type != 1, :] = 1
-    # print(mask) 看feature map
+
+    # mask[:, :, filter_type != 1, :] = 1
+    # print(mask)
+    # print(mask)  # 看feature map
     return mask
 
 
@@ -109,12 +112,18 @@ def np_mask(x):
     :return: an array
     """
     mask = getMask(x)
+    # print(mask)
     x_new = np.multiply(mask, x)
-    return np.maximum(x_new, 0)
+    return np.maximum(x_new, 0).astype(np.float32)
+
+# Test:
+# test = np.arange(1, 61)
+# test = np.reshape(test, [3, 2, 2, 5])
+# np_mask(test)
 
 
 # make the date type compatible with tensorflow
-np_mask_32 = lambda x: np_mask(x).astype(np.float32)
+# np_mask_32 = lambda x: np_mask(x).astype(np.float32)
 
 
 # The next three functions help us get a tf function to get gradients in bprop
@@ -173,23 +182,41 @@ def our_grad(cus_op, grad):
 
 # our final op
 def tf_mask(x, name=None):
-    with ops.name_scope(name, "Mask", [x]) as name:  # where bug is
-        z = py_func(np_mask_32,
+    with ops.name_scope(name, "Mask", [x]) as name:
+        z = py_func(np_mask,
                     [x],
                     [tf.float32],
                     name=name,
                     grad=our_grad)
-        # print(z)  # [<tf.Tensor 'OurRelu:0' shape=<unknown> dtype=float32>]
-        z = z[0]  # z is a tensor now
-        z.set_shape(x.get_shape())  # We need to give z a shape, in our context, it has the same shape as x
-        return z  # Tensor("OurRelu:0", dtype=float32)
 
-        # Test:
-        # with tf.Session() as sess:
-        #     x = tf.constant([[[1., 2., -3.]], [[7., -8., 9.]]])
-        #     x.eval()
-        #     z = tf_relu(x)
-        #     # z.set_shape(x.get_shape())
-        #     gr = tf.gradients(z, [x])[0]
-        #     tf.global_variables_initializer().run()
-        #     print(x.eval(), z.eval(), gr.eval())
+        z = z[0]  # z is a tensor now
+        # z.eval()
+        # Since py_func can execute arbitrary Python code and output anything, TensorFlow can't figure out the shape
+        # We will always have to manually set z's shape
+        z.set_shape(x.get_shape())  # We need to give z a shape, in our context, it has the same shape as x
+        # z = x
+        return z
+
+# Test:
+
+# with tf.Session() as sess:
+#             x = tf.constant([1., 2., 0.3, 0.1, -1., -1.5, 1.0, -8.0])
+#             # x = tf.constant([[[1., 2., -3., 5.]], [[7., -8., 9., -10.]]])
+#             x = tf.reshape(x, [2, 2, 2, 1])
+#             x.eval()
+#             # z = x
+#             z = tf_mask(x)
+#             # z.eval()
+#             gr = tf.gradients(z, [x])[0]
+#             tf.global_variables_initializer().run()
+#             print(x.eval(), z.eval(), gr.eval())
+
+
+# 几个改写的注意点：
+# 1 Python中的index从0开始
+# 2 关于shape, MATLAB中采用[h, w, depth, batchS]
+#   python中采用[batchS, h, w, depth]
+#   相应的sum的axis也要调整(from trial, np.sum(z,1) is equivalent to sum(z, 2) in MATLAB
+# 3 np.arange(1, h+1) 与 MATLAB中(1:h)是一样的，这也是python的特点
+# 4 shape的数字应该还是没问题的，实验了一下，[3,3]就是3*3
+# 5 延续上一点，与shape相关的index都是natural的理解方式，比如还有在linspace中给出的参数。
